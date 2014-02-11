62e94faba44c42f846270be91359ed41ff85dc27
ashleypeacock <avpeacock@gmail.com>
2014-02-11 01:12:05 +0000

    Just incase hd fails

---
+++ PMR/Course mindmap.mm
@@ -0,0 +1,23 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1391011313450" ID="ID_313115338" MODIFIED="1391011354908" TEXT="PMR">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1391011356893" ID="ID_1233544707" MODIFIED="1391011395341" POSITION="right" STYLE="bubble" TEXT="Probabalistic Graph Models">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1391011400628" ID="ID_1253934682" MODIFIED="1391011405175" TEXT="Belief Networks">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1391011409502" ID="ID_1266252097" MODIFIED="1391011414403" TEXT="Markov networks">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1391011418445" ID="ID_980558194" MODIFIED="1391011541709" TEXT="Factor graphs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
