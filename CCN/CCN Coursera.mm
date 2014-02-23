d1b97d17351884efb5d627350e452b54c95a5b35
ashleypeacock <avpeacock@gmail.com>
2014-02-23 23:21:06 +0000

    CCN notes

--- CCN/CCN Coursera.mm
+++ CCN/CCN Coursera.mm
@@ -3,7 +3,7 @@
 <node COLOR="#000000" CREATED="1392307871344" ID="ID_188406515" MODIFIED="1392307956016" STYLE="bubble" TEXT="CCN: Coursera">
 <font NAME="SansSerif" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
-<node COLOR="#0033ff" CREATED="1392307897774" ID="ID_893901258" MODIFIED="1392310984540" POSITION="right" TEXT="Week 1">
+<node COLOR="#0033ff" CREATED="1392307897774" FOLDED="true" ID="ID_893901258" MODIFIED="1393175417650" POSITION="right" TEXT="Week 1">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -24,8 +24,7 @@
       </li>
     </ul>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
 <node COLOR="#00b439" CREATED="1392308003083" ID="ID_1267832716" MODIFIED="1392311164577" TEXT="Receptive field">
@@ -46,8 +45,7 @@
       </li>
     </ul>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 <node COLOR="#990000" CREATED="1392308335976" ID="ID_1486546590" MODIFIED="1392310964872" TEXT="Descriptive model">
@@ -155,8 +153,7 @@
       </li>
     </ul>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="14"/>
 <node COLOR="#111111" CREATED="1392312160549" ID="ID_1261384831" MODIFIED="1392312556401" TEXT="Efficient coding algorithm">
 <richcontent TYPE="NOTE"><html>
@@ -179,14 +176,491 @@
       Algorithm gives rise to RFs that are optimised to be efficient and resconstruct patches in images as fatefully as possible.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
 <node COLOR="#00b439" CREATED="1392313618052" ID="ID_710617164" MODIFIED="1392316059150" TEXT="Neurobiology">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1393154500553" ID="ID_1518237048" MODIFIED="1393166342344" TEXT="Neurons">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Neuron is the fundemental structure and functional unit of the brain
+      </li>
+      <li>
+        They are discrete cells and not continuous with other cells.
+      </li>
+      <li>
+        Information flows from the dendrites to the axon via the cell body
+      </li>
+      <li>
+        The shape of neurons vary in some general way from one area of the brain to another
+      </li>
+    </ul>
+    <p>
+      Multiple EPSP inputs can sum to give an actional potential(or spike).
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1393166185517" ID="ID_1144301245" MODIFIED="1393166213075">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      EPSP: Excitatory Post-Synaptic Potential
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node COLOR="#111111" CREATED="1393166353027" ID="ID_681778078" MODIFIED="1393166523828" TEXT="What&apos;s in a neuron?">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Leaky bag of charged liquid.
+    </p>
+    <p>
+      Contents of the neuron are enclosed within a cell membrane which is a lipid(fat) bilayer.
+    </p>
+    <p>
+      The bilayer is impermeable to charged ion species such as Na+, CL- and K+.
+    </p>
+    <p>
+      Each neiron maintains a potential difference across its membrane. Inside is about -70mv relative to outside.
+    </p>
+    <p>
+      There are more Na+ and CL- outside, and more oganic ions higher inside (K+ and A-)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node COLOR="#111111" CREATED="1393166551634" ID="ID_630940410" MODIFIED="1393166672395" TEXT="Ionic channels: gate keepers">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      These allow sodium/chloring atoms to pass through the neuron's membrane. These gated channels allow communication between neurons.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Three different types:
+    </p>
+    <ul>
+      <li>
+        Voltage-gated
+      </li>
+      <li>
+        Chemically gated(synapses)
+      </li>
+      <li>
+        Mechanically gated: sensitive to pressure or stretch
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+<node COLOR="#111111" CREATED="1393166780175" ID="ID_1861149870" MODIFIED="1393166877870" TEXT="Neuronal signaling">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      When chemically-gated channels open, ions from the signalling neuron flow through.
+    </p>
+    <p>
+      This in turn causes the voltage to change and for the voltage gates to open, resulting in depolarization(positive change in voltage) or hyperolarization(negitive change in voltage).
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Strong enough depolarization causes a &quot;spike&quot; or &quot;action potential&quot;
+    </p>
+  </body>
+</html>
+</richcontent>
+<node COLOR="#111111" CREATED="1393166879302" ID="ID_1815219670" MODIFIED="1393166892804" TEXT="Action potential">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Strong enough depolarization causes a &quot;spike&quot; or &quot;action potential&quot;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1393170532442" ID="ID_1059569112" MODIFIED="1393170536245" TEXT="Nervous system">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1393170543474" ID="ID_1972554432" MODIFIED="1393170549805" TEXT="Peripheral Nervous System">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1393170567578" ID="ID_1920580663" MODIFIED="1393170651104" TEXT="Somatic">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Nerves connecting to voluntary skeletal muscles and sensory receptors(receptors on skin for example). Nerves are simply a bundle of axons.
+    </p>
+  </body>
+</html>
+</richcontent>
+<node COLOR="#111111" CREATED="1393170663656" ID="ID_1486501872" MODIFIED="1393170718389" TEXT="Afferent Nerve Fibers">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Axons that carry info away from the periphery to the CNS
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node COLOR="#111111" CREATED="1393170672064" ID="ID_72910304" MODIFIED="1393170729654" TEXT="Efferent Nerve Fibers">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Axons that carry infor from the CNS outward to the periphery
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1393170570968" ID="ID_130447077" MODIFIED="1393170813399" TEXT="Autonomic">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Nerves that connect to internal organs(heart, blood vessels, smooth miscles, fight or flight response etc). Operates below level of consciousness.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1393170552426" ID="ID_1495574296" MODIFIED="1393170557918" TEXT="Central Nervous System">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1393170559306" ID="ID_538868166" MODIFIED="1393172020675" TEXT="Brain">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      We don't yet fully know how each of the different sections of the brain interact. Might beable to work it out in 30-50 years!
+    </p>
+  </body>
+</html>
+</richcontent>
+<node COLOR="#111111" CREATED="1393171038301" ID="ID_755815718" MODIFIED="1393171041617" TEXT="Hindbrain">
+<node COLOR="#111111" CREATED="1393171054244" ID="ID_357817637" MODIFIED="1393171088411" TEXT="Medulla Oblongata">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Controls breathing, muscle tone and blood pressure
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node COLOR="#111111" CREATED="1393171093995" ID="ID_432256670" MODIFIED="1393171108641" TEXT="Pons">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Sleep and arousal. Connected to cellibellum.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node COLOR="#111111" CREATED="1393171110963" ID="ID_1195781737" MODIFIED="1393171139553" TEXT="Cerebellum">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Coordination and timing of voluntary movements, sense of equilibrium, language, attention
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1393171181442" ID="ID_1777036722" MODIFIED="1393171465829" TEXT="Midbrain">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Eye movements, visual and auditory reflexes
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node COLOR="#111111" CREATED="1393171476231" ID="ID_1144887880" MODIFIED="1393171545709">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Reticular formation
+    </p>
+  </body>
+</html>
+</richcontent>
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Modulates muscle reflexes, breathing and pain perception, sleep and wakefulness
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node COLOR="#111111" CREATED="1393171559566" ID="ID_1327038709" MODIFIED="1393171619032" TEXT="Thalamus">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Relay station for all sensory info to the cortex except for smell. Regulates sleep/wakefulness.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node COLOR="#111111" CREATED="1393171620661" ID="ID_35003813" MODIFIED="1393171665966" TEXT="Hypothalamus">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Regulates basic needs. Fighting, Fleeing, feeding and Mating.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node COLOR="#111111" CREATED="1393171683820" ID="ID_34600020" MODIFIED="1393171741662" TEXT="Cerebrum">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Contains cerebral cortex, basal ganglia, hippocampus and amydala
+    </p>
+    <p>
+      Involved in percpetion and motor control, cognitive function, learning, emotion, memory.
+    </p>
+  </body>
+</html>
+</richcontent>
+<node COLOR="#111111" CREATED="1393171741651" ID="ID_992979380" MODIFIED="1393171921183" TEXT="Cerebral cortex">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      What it means to be human. Skills such as language.
+    </p>
+    <p>
+      6 layers of neurons. Neurons are quite similar across area.
+    </p>
+    <p>
+      Different layers are used depending on the level of information and processing.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1393170562617" ID="ID_103388366" MODIFIED="1393170565453" TEXT="Spinal cord">
+<node COLOR="#111111" CREATED="1393170855262" ID="ID_482179074" MODIFIED="1393170878757" TEXT="Local feedback loops">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Control reflexes(reflex arcs). For example recoiling from a hot tray.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node COLOR="#111111" CREATED="1393170919413" ID="ID_1985644310" MODIFIED="1393170948919" TEXT="Decending motor control signals">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Activates spinal motor neurons.
+    </p>
+    <p>
+      Brain can tell neurons toimplement procedure for walking for example.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node COLOR="#111111" CREATED="1393170969325" ID="ID_1587011879" MODIFIED="1393171008936" TEXT="Ascending sensory axons">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Conveys sensory information from musscles and skin back to the brain
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1393175419111" ID="ID_1258732960" MODIFIED="1393175422967" POSITION="left" TEXT="Week 2">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1393175433291" ID="ID_1514976116" MODIFIED="1393175436858" TEXT="Neural code">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1393175506750" ID="ID_1148932668" MODIFIED="1393175513508" TEXT="Techniques for recording from the brain">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1393175793874" ID="ID_1028064121" MODIFIED="1393175797188" TEXT="Uninvasive">
+<node COLOR="#111111" CREATED="1393175618973" ID="ID_995675079" MODIFIED="1393175642833" TEXT="fMRI">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Placed inside a scanner with head between large magnet. Looks atblood flow of brain.
+    </p>
+  </body>
+</html></richcontent>
+<node COLOR="#111111" CREATED="1393175776803" ID="ID_417236565" MODIFIED="1393175784284" TEXT="Limitation: Cannot record individual neurons"/>
+</node>
+<node COLOR="#111111" CREATED="1393175672436" ID="ID_30216112" MODIFIED="1393175765410" TEXT="EEG">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Faster than fMRI but still requires averaging of neuron response times.
+    </p>
+    <p>
+      Downside: tends to be very noisey signal.
+    </p>
+    <p>
+      Non-invasive
+    </p>
+  </body>
+</html></richcontent>
+<node COLOR="#111111" CREATED="1393175767195" ID="ID_656299721" MODIFIED="1393175773892" TEXT="Limitation: Cannot record individual neurons"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1393175847306" ID="ID_1165338245" MODIFIED="1393175850340" TEXT="Invasive">
+<node COLOR="#111111" CREATED="1393175853026" ID="ID_1082582596" MODIFIED="1393175859844" TEXT="Electrode array"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1393175526878" ID="ID_489527355" MODIFIED="1393175543545">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Tools for discovering how the brain represents information
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1393175546509" ID="ID_1818110228" MODIFIED="1393175554013" TEXT="Models that express our understanding of this representation">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1393175560390" ID="ID_183560732" MODIFIED="1393175574549" TEXT="Some methods for interring what the brain is doing based on its activity(week 3)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1393182087855" ID="ID_183854567" MODIFIED="1393182212472" TEXT="Neural encoding: Simple models">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Response = single spike produced by a single neuron
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1393182260958" ID="ID_1640318069" MODIFIED="1393182271951" TEXT="Basic coding models">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1393182273629" ID="ID_1882114030" MODIFIED="1393182276599" TEXT="Linear response"/>
+<node COLOR="#111111" CREATED="1393182293716" ID="ID_1028492427" MODIFIED="1393182298336" TEXT="Temporal filtering"/>
+<node COLOR="#111111" CREATED="1393182768998" ID="ID_239602358" MODIFIED="1393182775171" TEXT="Spatial filtering"/>
+</node>
 </node>
 </node>
 </node>
