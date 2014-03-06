9d8feb4d9727aadb18264b1e2df631423dd22778
ashleypeacock <avpeacock@gmail.com>
2014-03-06 15:40:32 +0000

    Update

---
+++ ALE/mindmaps/Collaborative learning.mm
@@ -0,0 +1,354 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1393690467745" ID="ID_590111728" MODIFIED="1393690640189" STYLE="bubble" TEXT="Collaborative learning">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1393690818439" ID="ID_1051142904" MODIFIED="1393690867323" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Methods of
+    </p>
+    <p style="text-align: center">
+      analysis
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1393690924089" ID="ID_810554251" MODIFIED="1393690931511" TEXT="Conversational analysis">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1393691077963" ID="ID_398326206" MODIFIED="1393691084310" TEXT="Statistical discourse analysis">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1393691109284" ID="ID_315530022" MODIFIED="1393691111550" POSITION="left" TEXT="Thoughts">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1393691113175" ID="ID_14407047" MODIFIED="1393691190583" TEXT="Back to tribes">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      We've always collaborated in learning. For survival, to find food, to overcome problems. Is this an innate skill that we all have? Hence utalising this in learning could come very naturally.
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1393691201857" ID="ID_1048722456" MODIFIED="1393873829717" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Types
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1393691895493" ID="ID_1029091326" MODIFIED="1393691901732" TEXT="Collaborative Networked Learning">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1393692213988" ID="ID_596251719" MODIFIED="1393873818026" TEXT="Coursera">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1393876318546" ID="ID_98837961" MODIFIED="1393876322443" TEXT="Disadvantages">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1393876325942" ID="ID_1481845951" MODIFIED="1393876332311" TEXT="Need to have enough people!"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1393691903969" ID="ID_452341822" MODIFIED="1393691988082" TEXT="Computer-supported collaborative learning">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Uses technology in a learning environment to help mediate and support group interactions.
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1393692050671" ID="ID_587053177" MODIFIED="1393873818012" TEXT="Learning management system">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Tols which can assist or be assisted by others. Such tools include virtual classrooms, chat, discussion threads.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1393876460368" ID="ID_856631998" MODIFIED="1393876465945" TEXT="ITS">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1393876467536" ID="ID_1636704868" MODIFIED="1393876472218" TEXT="No need to wait for other people">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1393876473887" ID="ID_417991193" MODIFIED="1393877062523" TEXT="Removes some of the human disadvantages">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1393691367957" ID="ID_1742797860" MODIFIED="1393691373129" POSITION="left" TEXT="Perspectives">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1393691380381" ID="ID_1348894079" MODIFIED="1393691383162" TEXT="Advantages">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1393692109592" ID="ID_122411911" MODIFIED="1393692419267">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Development of
+    </p>
+    <p>
+      metacognition
+    </p>
+  </body>
+</html></richcontent>
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      How do we know what we don't know?
+    </p>
+    <ul>
+      <li>
+        Asking each other questions can help others understand their current gaps in knowledge as well as help others fill theirs.
+      </li>
+      <li>
+        The &quot;responsibility&quot; aspect may aid as an appropriate motivation for students whom were not otherwise motivated towards developing better metacognition.
+      </li>
+      <li>
+        In collaborative learning, the approach is just as important as the material, there seems to be an innate emphasis on metacognitive skills. How am I going to explain this? How am I going to communicate? How am I going to teach this to others?
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1393692304934" ID="ID_1904162906" MODIFIED="1393693442357">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Improves: retention of material,<br />motivation to learn,<br />time on tasks
+    </p>
+  </body>
+</html></richcontent>
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        More than 1200 studies sowing that students learnt considerably more than students working individually.
+      </li>
+      <li>
+        Optimal groups for learning tended to be three-four group members.
+      </li>
+      <li>
+        Lower-abled students best in mixed groups
+      </li>
+      <li>
+        Medium ability students best in homogeneous groups
+      </li>
+      <li>
+        Higher-level: no difference (errrm??? Surely put into a group of all lower-abilities is going to hinder if they focus on teaching what they already know rather than learning new things?)
+      </li>
+      <li>
+        Discussion based practices improved comprehension of text and critical-thinking skills.
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1393697261890" ID="ID_1180569764" MODIFIED="1393697266866" TEXT="Cues convergent thinking">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1393691384898" ID="ID_482651445" MODIFIED="1393691388652" TEXT="Disadvantages">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1393691506091" ID="ID_1882376010" MODIFIED="1393691577703">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Strong weight on
+    </p>
+    <p>
+      communication skills
+    </p>
+  </body>
+</html></richcontent>
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      If communication skills within the group are not good, this could hinder learning. However, it could also enhance learning as individuals may be motivated to improve communication in order to reach the common goal of greater understanding.
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1393693088357" ID="ID_1284650616" MODIFIED="1393693092342" TEXT="Can reply on others too much">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1393693093991" ID="ID_1559527527" MODIFIED="1393693099257" TEXT="&quot;Bad apples&quot; in the group">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1393697268623" ID="ID_1734068859" MODIFIED="1393697273522" TEXT="Hinders divergent thinking?">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1393691402072" ID="ID_1629640544" MODIFIED="1393691405229" TEXT="Neither/Both">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1393691406969" ID="ID_1915923472" MODIFIED="1393691501416">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Accountable for each
+    </p>
+    <p>
+      others learning.
+    </p>
+  </body>
+</html></richcontent>
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Don't want to let other people down
+      </li>
+      <li>
+        Motivation to learn better so you can help other people
+      </li>
+      <li>
+        Fear/anxiety of letting people down
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1393697321307" ID="ID_825901583" MODIFIED="1393697330858" POSITION="right" TEXT="Paper: Social cues for creativity">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1393697787073" ID="ID_695899082" MODIFIED="1393697792221" POSITION="left" TEXT="Questions">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1393697793939" ID="ID_1833399840" MODIFIED="1393697812980">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Link between metacognition and divergent/convergent thinking?
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1393698447355" ID="ID_1658313184" MODIFIED="1393698764397" TEXT="Could virtual collaboration help or hinder the process?">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Mimicry is a cue for for collaboration. If we remove this cue it could mean reducing the need or motivation to collaborate.
+      </li>
+      <li>
+        Convergent thinking is thought to occur when people are feeling wanted/comfortable in their social group.
+      </li>
+      <li>
+        Divergent thinking is thought to occur when in dissimilar or with disagreeable individuals.
+      </li>
+      <li>
+        Online collaboration removes some of the communication barriers and people may feel more intuned and thus initiate convergent type collaboration.
+      </li>
+      <li>
+        However, with the removal of&#160;being able to pick up on mimicry, the convergent signal may be missed?
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1393692500628" ID="ID_85970584" MODIFIED="1393704132489" TEXT="Is it adding to cognitive load?">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        It would make sense to me that this was. But normally when you increase cognitive load, you hinder learning or something in some other area.
+      </li>
+      <li>
+        This intuitively doesn't seem to be the case to me.
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1393875740523" ID="ID_1390112976" MODIFIED="1393875743203" POSITION="right" TEXT="Challenges">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
