11f9ba10adf2dde16438167b132ae88c62222369
ashleypeacock <avpeacock@gmail.com>
2014-03-19 19:18:17 +0000

    More dissertation updates

--- dissertation/Lit review.mm
+++ dissertation/Lit review.mm
@@ -10,8 +10,7 @@
       Lit review
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
 <node COLOR="#0033ff" CREATED="1394843162231" ID="ID_940747640" MODIFIED="1394846801313" POSITION="right">
@@ -24,18 +23,17 @@
       2. Sensory processing
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1394843665479" HGAP="27" ID="ID_1892848798" MODIFIED="1394846055351" VSHIFT="-21">
+<node COLOR="#00b439" CREATED="1394843665479" HGAP="27" ID="ID_1892848798" MODIFIED="1395247065348" VSHIFT="-21">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      Introduction: Effects on day to day functoning
+      Introduction: Prevelence in autism
     </p>
   </body>
 </html>
@@ -57,18 +55,40 @@
       </li>
     </ul>
   </body>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1394843672665" HGAP="22" ID="ID_404071821" MODIFIED="1395247408919" VSHIFT="23">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Experience: Effects on day to day functoning, more personal experience
+    </p>
+  </body>
 </html>
 </richcontent>
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-</node>
-<node COLOR="#00b439" CREATED="1394843672665" HGAP="22" ID="ID_404071821" MODIFIED="1394843704384" TEXT="Experience" VSHIFT="23">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#990000" CREATED="1394843677634" ID="ID_1560810426" MODIFIED="1394843680440" TEXT="Consequences">
+<node COLOR="#990000" CREATED="1394843677634" ID="ID_1560810426" MODIFIED="1395251714256" TEXT="Consequences">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        How to fit this in with: teachers challenging behaviours, sensory challenging behaviours, results of sensory stuff
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
 <font NAME="SansSerif" SIZE="14"/>
 <node COLOR="#111111" CREATED="1394843695585" ID="ID_710707851" MODIFIED="1394843698656" TEXT="Sensory overloads"/>
-<node COLOR="#111111" CREATED="1394843870335" ID="ID_537926671" MODIFIED="1394843874478" TEXT="Repetitive behaviours"/>
 <node COLOR="#111111" CREATED="1394846057591" ID="ID_395942473" MODIFIED="1394846066521">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -79,13 +99,17 @@
       Temperment, stress, etc
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node COLOR="#111111" CREATED="1394846751926" ID="ID_1596842070" MODIFIED="1394846756379" TEXT="Unusual fears"/>
+<node COLOR="#111111" CREATED="1395247420969" ID="ID_342340368" MODIFIED="1395247423478" TEXT="Meltdowns"/>
 </node>
 <node COLOR="#990000" CREATED="1394846121839" HGAP="25" ID="ID_58083574" MODIFIED="1394846765558" TEXT="Visual disturbences" VSHIFT="-46">
 <font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1395251716681" ID="ID_1454478750" MODIFIED="1395251719219" TEXT="coping">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1394843870335" ID="ID_537926671" MODIFIED="1394843874478" TEXT="Repetitive behaviours"/>
 </node>
 </node>
 </node>
@@ -99,8 +123,7 @@
       1. What is autism?
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
 <node COLOR="#00b439" CREATED="1394846295261" ID="ID_895561581" MODIFIED="1394846298683" TEXT="DSM-5">
@@ -111,7 +134,7 @@
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
-<node COLOR="#00b439" CREATED="1394846403726" ID="ID_1672655374" MODIFIED="1394846408976" TEXT="Public perception">
+<node COLOR="#00b439" CREATED="1395251558777" ID="ID_1812087749" MODIFIED="1395251567133" TEXT="repetitive behaviours">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
@@ -128,33 +151,54 @@
 <font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1394843891442" ID="ID_1728643466" MODIFIED="1394843897159" POSITION="left" TEXT="Effects of Autism">
-<edge STYLE="sharp_bezier" WIDTH="8"/>
-<font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1394843900660" ID="ID_288050013" MODIFIED="1394843906406" TEXT="Effects on chldren with ASD">
-<edge STYLE="bezier" WIDTH="thin"/>
-<font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#990000" CREATED="1394843942568" ID="ID_449995270" MODIFIED="1394844068689">
+<node COLOR="#0033ff" CREATED="1394843891442" ID="ID_1728643466" MODIFIED="1395247094907" POSITION="left">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      Effects at home
+      3. Effects of Autism
     </p>
   </body>
-</html></richcontent>
-<richcontent TYPE="NOTE"><html>
+</html>
+</richcontent>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1394843900660" ID="ID_288050013" MODIFIED="1395246715649">
+<richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
     <p>
-      Burden of care, stress in parents, etc
+      Effects on the home
     </p>
   </body>
-</html></richcontent>
+</html>
+</richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1395246722374" ID="ID_412858359" MODIFIED="1395247049554" TEXT="Parent stress">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Burden of care.
+      </li>
+      <li>
+        Public perception
+      </li>
+      <li>
+        Embarassment
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
 <font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
@@ -163,7 +207,7 @@
 <font NAME="SansSerif" SIZE="16"/>
 <node COLOR="#990000" CREATED="1394843919856" ID="ID_600457129" MODIFIED="1394843923174" TEXT="Effects on teachers">
 <font NAME="SansSerif" SIZE="14"/>
-<node COLOR="#111111" CREATED="1394846868572" ID="ID_341065938" MODIFIED="1394846872197" TEXT="Challenging behaviours"/>
+<node COLOR="#111111" CREATED="1394846868572" ID="ID_341065938" MODIFIED="1395246660650" TEXT="Challenging behaviours"/>
 </node>
 <node COLOR="#990000" CREATED="1394843924916" ID="ID_874726498" MODIFIED="1394846224363" TEXT="Effects on children with ASD">
 <richcontent TYPE="NOTE"><html>
@@ -183,11 +227,19 @@
       </li>
     </ul>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
+</node>
+<node COLOR="#0033ff" CREATED="1395247082322" ID="ID_827409291" MODIFIED="1395247085056" POSITION="right" TEXT="Meltdowns">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1395247428712" HGAP="-69" ID="ID_399936986" MODIFIED="1395251714255" POSITION="left" TEXT="Challenging behaviours" VSHIFT="160">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<arrowlink DESTINATION="ID_1560810426" ENDARROW="Default" ENDINCLINATION="1217;0;" ID="Arrow_ID_1100187871" STARTARROW="None" STARTINCLINATION="1217;0;"/>
+<font NAME="SansSerif" SIZE="18"/>
 </node>
 </node>
 </map>
